49b1b7f7e7e4d9ec2d2a7e906cfdd0a4385b5095
heric <herickdan@hotmail.com>
2024-07-09 23:47:13 -0300

    Updating with react files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Caralho eu tenho que tomar no cu mesmo
+      </h1>
+    </div>
+  );
+}
+
+export default App;
