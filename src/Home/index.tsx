bb9b775ba35d4d2e83e48c45ca9de6b4976b6114
heric <herickdan@hotmail.com>
2024-07-10 00:14:44 -0300

    start of the home page

---
+++ src/Home/index.tsx
@@ -0,0 +1,11 @@
+import shoppingImage from '../imgs/shopping.jpg'
+
+export const Home = () =>{
+ return(
+    <>
+    <div>
+     <img src={shoppingImage} style={{width:'97vw', height:'100vh'}}/>
+    </div>
+</>
+ )
+}
