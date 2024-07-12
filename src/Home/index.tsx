5b29b1a6189340a28485dcb070dfa00fe811f53c
heric <herickdan@hotmail.com>
2024-07-12 00:06:09 -0300

    Home Page Style

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,10 +1,18 @@
 import shoppingImage from '../imgs/shopping.jpg'
+import { styles } from './style'
 
 export const Home = () =>{
  return(
     <>
-    <div>
-     <img src={shoppingImage} style={{width:'97vw', height:'100vh'}}/>
+    <div style={styles.backGround}>
+        <div>
+        <h1 style={styles.title}>Lista de compras</h1>
+        <h2 style={styles.subTitle}>Organize suas compras com facilidade e eficiência</h2>
+        <div>
+        <button style={styles.addItemButton}>Adicionar Item</button>
+        <button style={styles.viewListButton}>Vizualizar Item</button>
+        </div>
+        </div>
     </div>
 </>
  )
---
+++ src/Home/style.ts
@@ -0,0 +1,44 @@
+import shoppingImage from '../imgs/shopping.jpg'
+
+export const styles ={
+    backGround:{
+        margin:'0',
+        padding:'0',
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url(${shoppingImage}) `,
+        backgroundSize:'cover',
+        height:'100vh',
+        display:'flex',
+        paddingLeft:'200px',
+        alignItems:'center'
+    },
+    title:{
+        color:'#F5F0F0',
+        fontSize:'70px', 
+        fontFamily: 'Roboto',
+    },
+    subTitle:{
+        color:'#F5F0F0',
+        fontSize:'23px',  fontFamily: 'Roboto',
+    },
+    addItemButton:{
+        backgroundColor:'#027fe9',
+        color:'black',
+        border:'none',
+        width:'135px',
+        height:'50px',
+        fontSize:'18px',
+        padding:'5px',
+        borderRadius:'10px'    
+    },
+    viewListButton:{
+        backgroundColor:'black',
+        color:'#027fe9',
+        border:'solid 2px #027fe9',
+        width:'135px',
+        height:'50px',
+        fontSize:'18px',
+        padding:'5px',
+        borderRadius:'10px',
+        marginLeft:'20px'    
+    }
+}
