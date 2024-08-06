bcfed8b79d4c645ba5f5aa42591fdbe36a51e526
heric <herickdan@hotmail.com>
2024-08-05 22:51:13 -0300

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
 import './App.css';
 import { Home } from './Home';
-import { ModalWidthForm } from './Modal';
-
 function App() {
-  
   return (
     <div className="App">
       <Home/>
