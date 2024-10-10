b7c017e7f150821e3d212be5a7dc1431d114f86a
heric <herickdan@hotmail.com>
2024-10-10 15:30:41 -0300

    necessity part

---
+++ src/Necessidade/index.tsx
@@ -0,0 +1,17 @@
+import {styles} from './style'
+
+export const Necessity = () =>{
+    return(
+        <div style={styles.necessityBox}> 
+            <h1 style={styles.title}>
+                Nós entendemos o seu problema
+            </h1>
+            <p  style={styles.text}>
+                Nosso produto foi desenvolvido para oferecer uma solução prática e eficiente para gerenciar suas compras.
+                Com uma interface simples e intuitiva, você pode facilmente adicionar, 
+                excluir e editar itens, garantindo uma experiência rápida e sem complicações. Sabemos que o seu tempo é valioso, 
+                e é por isso que nossa ferramenta visa tornar o processo o mais fácil e ágil possível.
+            </p>
+        </div>
+    )
+}
---
+++ src/Necessidade/style.ts
@@ -0,0 +1,19 @@
+export const styles: { [keys: string]: React.CSSProperties } = {
+    necessityBox:{
+        width:'60%',
+        height:'90vh',
+        display:'flex',
+        margin:'auto',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        transition:'0.2s'
+    },
+    title:{
+        color:'rgb(35,156,80)', 
+        fontSize:'3em'
+    },
+    text:{
+        fontSize:'25px'
+    }
+};
