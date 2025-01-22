552c6e7ccd5c9ee965c38b30f4b6b65674d75bcc
heric <herickdan@hotmail.com>
2025-01-21 22:31:26 -0300

    use contextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import { UserContext } from './context';
 import { Home } from './Home';
 function App() {
+  const [modal, setModal] = useState(false);
+  const [nome, setNome] = useState('')
+  const [quantidade, setQuantidade] = useState<number | undefined>(undefined); 
+  const [homeState, setHomeState] = useState(true);
   return (
+   <UserContext.Provider value={{modal, setModal, nome, setNome, quantidade, setQuantidade, homeState, setHomeState}}>
     <div className="App">
       <Home/>
     </div>
+   </UserContext.Provider>
   );
 }
 
